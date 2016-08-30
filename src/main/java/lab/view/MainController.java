448a9cd1b7dfb58f5b884fc7fefe57161f933d9a
leoutn92 <leoutn92@gmail.com>
2016-08-30 00:46:50 -0300

    Primer commit

---
+++ src/main/java/lab/view/MainController.java
@@ -0,0 +1,41 @@
+package lab.view;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean
+@ViewScoped
+public class MainController implements Serializable {
+
+	private static final long serialVersionUID = 3973801993975443027L;
+
+	private String name;
+	private String primeName;
+	private String richName;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrimeName() {
+		return primeName;
+	}
+
+	public void setPrimeName(String primeName) {
+		this.primeName = primeName;
+	}
+
+	public String getRichName() {
+		return richName;
+	}
+
+	public void setRichName(String richName) {
+		this.richName = richName;
+	}
+}
