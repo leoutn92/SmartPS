78337d198ee7079f84b6002bcf9b4504a1a97b44
matiseni51 <matiseni51@hotmail.com>
2016-09-05 15:20:47 -0300

    PrimerLineaDao-AlumnoDAO semi implementado

--- src/main/java/com/smartps/dao/AlumnoDAO.java
+++ src/main/java/com/smartps/dao/AlumnoDAO.java
@@ -1,23 +1,26 @@
 package com.smartps.dao;
 
 //import java.util.List;
+import javax.persistence.EntityManager;
 
 import com.smartps.model.Alumno;
 import com.smartps.dao.IAlumnoDAO;
 
 public class AlumnoDAO implements IAlumnoDAO {
 
-
+	EntityManager entitymanager;
+	
+	
 	public void save(Alumno alumno){
-		
+		entitymanager.persist(alumno);
 	}
 	
 	public void update(Alumno alumno){
-		
+		entitymanager.merge(alumno);
 	}
 	
 	public void delete(Alumno alumno){
-		
+		entitymanager.remove(alumno);
 	}
 	
 //	public List<Alumno> retriveALL(){}
